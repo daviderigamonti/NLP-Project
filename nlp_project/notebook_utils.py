6bf1a79458ac5e5b957c1787b8bee8039f3541be
davide-rigamonti-polimi <davide2.rigamonti@mail.polimi.it>
2023-05-13 22:58:04 +0200

    Updated split interface and solved data leakage

--- nlp_project/notebook_utils.py
+++ nlp_project/notebook_utils.py
@@ -10,17 +10,24 @@ from sklearn.metrics import (
 )
 from sklearn.utils import resample
 
+from os.path import exists, isfile
+from joblib import dump, load
 
-def split(x, y, test_size=0.2, val_size=0.0):
+
+def split(x, y, test_size=0.2, val_size=0.0, seed=0):
     if val_size + test_size >= 1:
         return None
     x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=test_size + val_size, stratify=y
+        x, y, test_size=test_size + val_size, stratify=y, random_state=seed
     )
     x_val, y_val = None, None
     if val_size > 0:
         x_test, x_val, y_test, y_val = train_test_split(
-            x_test, y_test, test_size=val_size / (test_size + val_size), stratify=y_test
+            x_test,
+            y_test,
+            test_size=val_size / (test_size + val_size),
+            stratify=y_test,
+            random_state=seed,
         )
     return x_train, x_val, x_test, y_train, y_val, y_test
 
@@ -31,3 +38,17 @@ def evaluate(y_true, y_pred, labels=None):
     cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
     cm_display.plot()
     plt.show()
+
+
+def save_scikit_model(path, model, name):
+    dump(model, path + "/" + name)
+
+
+def load_scikit_model(path, name):
+    model_path = path + "/" + name
+    if exists(model_path) and isfile(model_path):
+        try:
+            return load(model_path)
+        except:
+            pass
+    return None
